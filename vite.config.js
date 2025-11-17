cdb031bf8226a71960684fc005f34b23ecc25a72
Puneeth <84642457+Narra-Puneeth@users.noreply.github.com>
2025-11-18 00:49:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'https://aura-backend-ten.vercel.app',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^/api/, '/api')
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   }
